bb3d589ae4bf1773e059914c88fd89068d63995d
Xiaozekun <xiaozekun2017@gmail.com>
2019-02-18 18:33:25 +0800

    change settings

--- blog/settings.py
+++ blog/settings.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 class BaseConfig():
-    SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
