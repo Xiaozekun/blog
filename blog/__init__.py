5def4a11405d03f806356e6ae7243fc97ba81f31
Xiaozekun <xiaozekun2017@gmail.com>
2019-02-19 18:19:57 +0800

    ready

--- blog/__init__.py
+++ blog/__init__.py
@@ -163,15 +163,15 @@ def register_logging(app):
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.INFO)
 
-    mail_handler = SMTPHandler(
-        mailhost=app.config['MAIL_SERVER'],
-        fromaddr=app.config['MAIL_USERNAME'],
-        toaddrs=['ADMIN_EMAIL'],
-        subject='Blog Application Error',
-        credentials=(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']))
-    mail_handler.setLevel(logging.ERROR)
-    mail_handler.setFormatter(request_formatter)
+    # mail_handler = SMTPHandler(
+    #     mailhost=app.config['MAIL_SERVER'],
+    #     fromaddr=app.config['MAIL_USERNAME'],
+    #     toaddrs=['ADMIN_EMAIL'],
+    #     subject='Blog Application Error',
+    #     credentials=(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']))
+    # mail_handler.setLevel(logging.ERROR)
+    # mail_handler.setFormatter(request_formatter)
 
     if not app.debug:
-        app.logger.addHandler(mail_handler)
+        # app.logger.addHandler(mail_handler)
         app.logger.addHandler(file_handler)
